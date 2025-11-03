f41e84bf4c7e698b521d1c78287bd1c49dfb05aa
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-11-02 17:11:00 -0800

    The Great Purge

--- Assets/Scripts/Core/HubPortal.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class HubPortal : MonoBehaviour {
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
-            GameLoop.I.EnterArena();
-    }
-}
